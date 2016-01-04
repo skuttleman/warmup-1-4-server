c82b9e001bf757f8042060abb1cadeb28a3a0188
Ben Allred <skuttleman@gmail.com>
2016-01-04 09:42:35 -0700

    First version of server going

--- index.js
+++ index.js
@@ -4,23 +4,16 @@ try {
 	console.error(err);
 }
 var express = require('express'), app = express();
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var cors = require('cors');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
-// app.use(cors());
-
-
-var crudl = require('./routes/crudl');
-//app.use('/route', crudl('<table_name>', ['column1', 'column2', 'column3']));
-
-app.get('/*', function(request, response) {
-	response.send('I am here to serve you.');
+app.get('/', function(request, response) {
+	knex('broncos').then(function(broncos) {
+		response.json({ broncos: broncos });
+	}).catch(function(err) {
+		console.error(err);
+		response.err(err);
+	});
 });
 
 app.listen(process.env.PORT || 8000, function() {
   console.log('The NSA is listening on port', process.env.PORT || 8000);
 });
-
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,10 @@
 module.exports = {
   development: {
     client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/warmup_1_4'
+  },
+  production : {
+    client: 'pg',
     connection: process.env.DATABASE_URL
   }
 }
