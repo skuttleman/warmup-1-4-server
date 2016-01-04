f58a40beacea6b0e1b9727812736214d6e7db0b6
Ben Allred <skuttleman@gmail.com>
2016-01-04 10:11:05 -0700

    updated production db url

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,6 @@ module.exports = {
   },
   production : {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL || 'postgres://atlmfsatihlmys:Xd1_3bip8DcdhnkHF9Juxh9dhn@ec2-54-235-147-211.compute-1.amazonaws.com:5432/d14ff6qa474qg4?ssl=true'
   }
 }
